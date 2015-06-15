1cfa5452cf03466c79a6633724e4cb14600051a7
Mike Sergeant <sergeant.mj@gmail.com>
2015-06-14 22:00:40 -0400

    Pencil marks are removed from numbers in the same row

--- js/SudokuPencilMarks.js
+++ js/SudokuPencilMarks.js
@@ -1,17 +1,42 @@
 var SudokuPencilMarks = {
   create: function(sudokuBoard){
-    var board = sudokuBoard;
+    var board = sudokuBoard.values();
     var rawMarks = [];
+
+    function removeElement(array, value){
+      var index = array.indexOf(value);
+      if(index > -1){
+        array.splice(index, 1);
+      }
+    }
+
+    function eliminateRowValues(values, row){
+      for(var col = 0; col < 9; col++){
+        var rowVal = board[row][col];
+        if(rowVal > 0){
+          removeElement(values, rowVal);
+        }
+      }
+    }
 
     var marks = function() {};
 
     marks.values = function(){
       rawMarks = [];
 
-      for(row = 0; row < 9; row++){
+      for(var row = 0; row < 9; row++){
         rawMarks.push([[],[],[],[],[],[],[],[],[]]);
-        for(col = 0; col < 9; col++){
-          rawMarks[row][col] = [1,2,3,4,5,6,7,8,9];
+      }
+
+      for(row = 0; row < 9; row++){
+        for(var col = 0; col < 9; col++){
+          if(board[row][col] == 0){
+            var remainingValues = [1,2,3,4,5,6,7,8,9];
+
+            eliminateRowValues(remainingValues, row);
+
+            rawMarks[row][col] = remainingValues;
+          }
         }
       }
 
--- spec/SudokuPencilMarks_values.js
+++ spec/SudokuPencilMarks_values.js
@@ -6,5 +6,37 @@ describe("SudokuPencilMarks.values", function(){
     expect(marks[0][0]).toEqual([1,2,3,4,5,6,7,8,9]);
   });
 
+  it("returns empty if the value has already been set", function(){
+    var boardString = stringFromBoardArray([[0, 1, 2, 3, 4, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0]]);
+    var board = SudokuBoard.create(boardString);
+    var marks = SudokuPencilMarks.create(board).values();
+
+    expect(marks[0][1]).toEqual([]);
+  });
+
+  it("takes out values that have already been used in that row", function(){
+    var boardString = stringFromBoardArray([[0, 1, 2, 3, 4, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0]]);
+    var board = SudokuBoard.create(boardString);
+    var marks = SudokuPencilMarks.create(board).values();
+
+    expect(marks[0][0]).toEqual([5,6,7,8,9]);
+  });
+
 });
 
