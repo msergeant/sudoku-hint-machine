69273ae6d4dd4b02e02259b43681ab36b2a1be7d
Mike Sergeant <sergeant.mj@gmail.com>
2017-01-26 21:55:34 -0500

    Finish column swap functionality

--- spec/SudokuPermuter_columnSwap.js
+++ spec/SudokuPermuter_columnSwap.js
@@ -11,17 +11,56 @@ describe("SudokuPermuter.columnSwap", function(){
                      "723456789",
                      "823456789",
                      "923456789"].join('');
-    var newString = SudokuPermuter.columnSwap(oldString, 0, 1);
 
-    expect(newString).toEqual(["213456789",
-                               "223456789",
-                               "233456789",
-                               "243456789",
-                               "253456789",
-                               "263456789",
-                               "273456789",
-                               "283456789",
-                               "293456789"].join(''));
+    expect(SudokuPermuter.columnSwap(oldString, 0, 1))
+      .toEqual(["213456789",
+                "223456789",
+                "233456789",
+                "243456789",
+                "253456789",
+                "263456789",
+                "273456789",
+                "283456789",
+                "293456789"].join(''));
   });
+
+  it("works if column numbers are reversed", function() {
+    var oldString = ["123456789",
+                     "223456789",
+                     "323456789",
+                     "423456789",
+                     "523456789",
+                     "623456789",
+                     "723456789",
+                     "823456789",
+                     "923456789"].join('');
+
+    expect(SudokuPermuter.columnSwap(oldString, 4, 2))
+      .toEqual(["125436789",
+                "225436789",
+                "325436789",
+                "425436789",
+                "525436789",
+                "625436789",
+                "725436789",
+                "825436789",
+                "925436789"].join(''));
+  });
+
+  it("does nothing if columns are the same", function() {
+    var oldString = ["123456789",
+                     "223456789",
+                     "323456789",
+                     "423456789",
+                     "523456789",
+                     "623456789",
+                     "723456789",
+                     "823456789",
+                     "923456789"].join('');
+
+    expect(SudokuPermuter.columnSwap(oldString, 5, 5))
+      .toEqual(oldString);
+  });
+
 
 });
--- src/js/SudokuPermuter.js
+++ src/js/SudokuPermuter.js
@@ -5,6 +5,23 @@ var SudokuPermuter = {
     return originalString.replace(regex1, '?').replace(regex2, num1).replace(/\?/g, num2);
   },
   columnSwap: function(originalString, col1, col2) {
+    function swapChar(str, index1, index2) {
+      return str.substr(0, index1)
+             + str[index2]
+             + str.substring(index1+1, index2)
+             + str[index1]
+             + str.substr(index2+1);
+    }
+
+    if(col1 === col2) {
+      return originalString;
+    }
+
+    const [first, last] = [col1, col2].sort();
+
+    return originalString.match(/.{1,9}/g).map(function(str) {
+      return swapChar(str, first, last);
+    }).join('');
   }
 };
 
